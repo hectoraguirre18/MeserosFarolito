6dea348bc1b535a3771bf991e4153db531f3f33f
LAPTOP-SICU8JHI\Hector <hector_scooby321@hotmail.com>
2017-11-22 12:17:59 -0700

    New waitlist fragment design

--- MeserosFarolito/app/src/main/java/com/farolito/meseros/meserosfarolito/MainActivity.java
+++ MeserosFarolito/app/src/main/java/com/farolito/meseros/meserosfarolito/MainActivity.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
@@ -27,6 +29,11 @@ public class MainActivity extends AppCompatActivity implements TablesFragment.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
 
         android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
--- MeserosFarolito/app/src/main/java/com/farolito/meseros/meserosfarolito/WaitlistFragment.java
+++ MeserosFarolito/app/src/main/java/com/farolito/meseros/meserosfarolito/WaitlistFragment.java
@@ -1,14 +1,24 @@
 package com.farolito.meseros.meserosfarolito;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -26,6 +36,10 @@ public class WaitlistFragment extends Fragment {
     private static final String ARG_PARAM2 = "param2";
 
     private Waitlist_adapter mAdapter;
+
+    TextView TextViewCount;
+
+    private String client_count = "";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -89,6 +103,10 @@ public class WaitlistFragment extends Fragment {
 
         mRecyclerView.setAdapter(mAdapter);
 
+        TextViewCount = (TextView) v.findViewById(R.id.count_tv);
+
+        updateCounter();
+
         v.findViewById(R.id.waitlist_fab).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -97,8 +115,141 @@ public class WaitlistFragment extends Fragment {
             }
         });
 
+        final EditText nameEditText = (EditText) v.findViewById(R.id.client_name_edittext);
+
+        v.findViewById(R.id.addClient_btn_0).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!TextUtils.isEmpty(client_count)){
+                    client_count = client_count + "0";
+                    updateCounter();
+                }
+            }
+        });
+        v.findViewById(R.id.addClient_btn_1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                client_count = client_count + "1";
+                updateCounter();
+            }
+        });
+        v.findViewById(R.id.addClient_btn_2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                client_count = client_count + "2";
+                updateCounter();
+            }
+        });
+        v.findViewById(R.id.addClient_btn_3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                client_count = client_count + "3";
+                updateCounter();
+            }
+        });
+        v.findViewById(R.id.addClient_btn_4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                client_count = client_count + "4";
+                updateCounter();
+            }
+        });
+        v.findViewById(R.id.addClient_btn_5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                client_count = client_count + "5";
+                updateCounter();
+            }
+        });
+        v.findViewById(R.id.addClient_btn_6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                client_count = client_count + "6";
+                updateCounter();
+            }
+        });
+        v.findViewById(R.id.addClient_btn_7).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                client_count = client_count + "7";
+                updateCounter();
+            }
+        });
+        v.findViewById(R.id.addClient_btn_8).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                client_count = client_count + "8";
+                updateCounter();
+            }
+        });
+        v.findViewById(R.id.addClient_btn_9).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                client_count = client_count + "9";
+                updateCounter();
+            }
+        });
+
+        v.findViewById(R.id.addClient_btn_backspace).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!TextUtils.isEmpty(client_count)){
+                    client_count = removeLastChar(client_count);
+                }
+                updateCounter();
+            }
+        });
+
+        ImageButton btn_done = (ImageButton) v.findViewById(R.id.addClient_btn_done);
+
+        btn_done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(TextUtils.isEmpty(nameEditText.getText().toString())){
+                    nameEditText.setError("Por favor introduzca un nombre");
+                    nameEditText.requestFocus();
+                } else if(TextUtils.isEmpty(client_count)){
+                    Toast.makeText(getContext(), "Por favor introduzca un numero de personas", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    int count = Integer.parseInt(client_count);
+                    String name = nameEditText.getText().toString();
+
+                    WaitlistDBHelper mDbHelper = new WaitlistDBHelper(getContext());
+                    SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+                    ContentValues values = new ContentValues();
+                    values.put(WaitlistContract.WaitlistEntry.COLUMN_CLIENT_NAME, name);
+                    values.put(WaitlistContract.WaitlistEntry.COLUMN_CLIENT_COUNT, count);
+
+                    db.insert(WaitlistContract.WaitlistEntry.WAITLIST_TABLE_NAME, null, values);
+
+                    nameEditText.setText("");
+                    client_count = "";
+                    updateCounter();
+                    updateList();
+                }
+            }
+        });
+
 
         return v;
+    }
+
+    public static String removeLastChar(String s) {
+        return (s == null || s.length() == 0)
+                ? null
+                : (s.substring(0, s.length() - 1));
+    }
+
+    private void updateCounter(){
+        String auxString = getContext().getResources().getString(R.string.client_count);
+        if(TextUtils.isEmpty(client_count)){
+            TextViewCount.setText(auxString + " 0");
+        } else {
+            TextViewCount.setText(auxString + " " + client_count);
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
